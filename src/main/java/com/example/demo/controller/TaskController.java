4d4901159e1b2779a31d3668b4a0aaf76f950a7c
balqw <balqw777@gmail.com>
2021-05-08 01:24:29 +0300

    hotfix

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.entity.Task;
 import com.example.demo.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/task")
 public class TaskController {
     private final TaskService taskService;
 
@@ -16,6 +22,18 @@ public class TaskController {
 
     @GetMapping
     public String getNewTask(){
+        return "day/index";
+    }
+
+    @GetMapping("/new")
+    public String getCreatePage(Model model){
+        model.addAttribute("task",new Task());
+        return "day/new";
+    }
+
+    @PostMapping
+    public String saveTask(@ModelAttribute("task") Task task){
+        taskService.createTask(task);
         return "day/index";
     }
 
